a0a6cd5274abba5d0610f17b436ff7a6ceb3df2a
Matthew Lin <mlin367@yahoo.com>
2018-10-03 14:29:01 -0700

    Finish React components sprint

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryListItems groceryItems = {['Apples', 'Oranges']} />
+  </div>
+);
+
+var Apples = () => (
+  <li>Apples!</li>
+)
+
+var Oranges = () => (
+  <li>Oranges!</li>
+)
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var GroceryListItems = (props) => {
+
+  return (
+    <ul>
+      {props.groceryItems.map(groceryItem => 
+        <GroceryListItem groceryItem={groceryItem} />
+      )}
+    </ul>
+  )
+};
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
